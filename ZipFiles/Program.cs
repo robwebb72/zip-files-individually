d30011a27c738ba808423e8aa5516522a427ffc1
robstah72@gmail.com <robstah72@gmail.com>
2024-05-23 10:34:11 +0100

    Add argument use.  Refactored code.

--- ZipFiles/Program.cs
+++ ZipFiles/Program.cs
@@ -1,36 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.IO.Compression;
+﻿using System.IO.Compression;
 
 namespace ZipFiles;
 
 public static class Program
 {
-    public static void Main()
+    public static void Main(string [] args)
     {
-        var path = @"d:\test";
+        var (path, extensionList) = ProcessArguments(args);
+        if (path == string.Empty)
+            return;
 
-        var files =Directory.EnumerateFiles(
-            path, 
-            "*.*", 
-            SearchOption.TopDirectoryOnly);
-
-        var validExtensions = new List<string> {".lha"};
+        var files = Directory.EnumerateFiles(path, "*.*", SearchOption.TopDirectoryOnly);
 
         foreach (var file in files)
         {
             var extension = Path.GetExtension(file);
-            var fileName = Path.GetFileNameWithoutExtension(file);
-            var filePath = Path.GetDirectoryName(file);
-        
-            if(!validExtensions.Contains(extension.ToLower())) continue;
-            var zipFileName = $"{filePath}{Path.DirectorySeparatorChar}{fileName}.zip";
-            if (File.Exists(zipFileName)) continue;
-            using var zip = ZipFile.Open(zipFileName, ZipArchiveMode.Create);
-            zip.CreateEntryFromFile(file, Path.GetFileName(file));
-            File.Delete(file);
+            if(extensionList.Contains(extension.ToLower()))
+                CreateZipFile(file);
         }
         Console.WriteLine("Finished.");    
     }
-}
 
+    private static (string, List<string>) ProcessArguments(string[] arguments)
+    {
+        if (arguments.Length != 2)
+        {
+            Console.WriteLine(
+                "usage is zip-files <extension-list> <path>\n\n" + 
+                "where extension list is a comma separated list of extensions to zip."
+            );
+            
+            return (string.Empty, []);
+        }
+
+        var extensions = arguments[0].Split(",").Select(a => a.Trim().TrimStart('.')).Select(b => "." + b).ToList();
+
+        return (arguments[1], extensions);
+    }
+
+    private static void CreateZipFile(string file)
+    {
+        var zipFileName = 
+            $"{Path.GetDirectoryName(file)}{Path.DirectorySeparatorChar}{Path.GetFileNameWithoutExtension(file)}.zip";
+            
+        Console.Write($"zipping   {file}...  ");
+        if (File.Exists(zipFileName))
+        {
+            Console.WriteLine("not written. zip file already exists.");
+            return;
+        }
+            
+        using var zip = ZipFile.Open(zipFileName, ZipArchiveMode.Create);
+        zip.CreateEntryFromFile(file, Path.GetFileName(file));
+        File.Delete(file);
+        Console.WriteLine("done.");
+    }
+}
